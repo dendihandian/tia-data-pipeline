de136b2bb04d477f77c52aceb6da857a17d3afcb
Dendi Handian <contact.dendi@yahoo.com>
2022-06-08 03:28:20 +0700

    draft pipeline

--- docker/airflow/dags/tia_pipeline.py
+++ docker/airflow/dags/tia_pipeline.py
@@ -1,14 +1,13 @@
 import airflow
 import requests
-import logging
 import json
-from airflow import settings
+import pandas as pd
 from airflow import DAG
-# from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.sensors.http_sensor import HttpSensor
 from airflow.models.connection import Connection
 from datetime import datetime, timedelta
+from sqlalchemy import create_engine
 
 default_args = {
     "owner": "Airflow",
@@ -25,7 +24,41 @@ def save_latest_posts_to_json():
     json_fname = datetime.now().strftime("%Y-%m-%d-%H") + ".json"
     with open(f'/opt/airflow/json/posts/{json_fname}', 'w') as outfile:
         r = requests.get('https://www.techinasia.com/wp-json/techinasia/2.0/posts?page=1&per_page=30', headers={'Content-Type': 'application/json', 'User-Agent': 'Airflow'})
-        outfile.write(json.dumps(r.json()))
+        outfile.write(json.dumps(r.json()['posts']))
+
+def transform_posts_json():
+    json_fname = datetime.now().strftime("%Y-%m-%d-%H") + ".json"
+
+    data = None
+    with open(f'/opt/airflow/json/posts/{json_fname}', 'r') as file_content:
+        data = json.load(file_content)
+
+    for index, post in enumerate(data):
+        data[index]['date_gmt'] = post['date_gmt'].replace('T', ' ').replace('Z', '')
+        data[index]['modified_gmt'] = post['modified_gmt'].replace('T', ' ').replace('Z', '')
+        data[index]['word_count'] = len(post['content'].split())
+        data[index]['vsitems'] = str(post['vsitems'])
+        data[index]['live_items'] = str(post['live_items'])
+        data[index]['author'] = str(post['author'])
+        data[index]['comments'] = str(post['comments'])
+        data[index]['featured_image'] = str(post['featured_image'])
+        data[index]['post_images'] = str(post['post_images'])
+        data[index]['seo'] = str(post['seo'])
+        data[index]['categories'] = str(post['categories'])
+        data[index]['tags'] = str(post['tags'])
+        data[index]['companies'] = str(post['companies'])
+        data[index]['sponsor'] = str(post['sponsor'])
+        data[index]['external_scripts'] = str(post['external_scripts'])
+
+    with open(f'/opt/airflow/json/posts/{json_fname}', 'w') as outfile:
+        outfile.write(json.dumps(data))
+
+def load_posts_data_to_postgres():
+    json_fname = datetime.now().strftime("%Y-%m-%d-%H") + ".json"
+    posts = pd.read_json(f"/opt/airflow/json/posts/2022-06-07-19.json")
+
+    engine = create_engine('postgresql://airflow:airflow@postgres:5432/airflow_db')
+    posts.head(1).to_sql('posts', con=engine, if_exists='append', index=False)
 
 
 with DAG(dag_id="tia_pipeline", schedule_interval="@hourly", default_args=default_args, catchup=False) as dag:
@@ -40,11 +73,23 @@ with DAG(dag_id="tia_pipeline", schedule_interval="@hourly", default_args=defaul
         response_check=lambda response: "posts" in response.text
     )
 
-    # load last hour of posts from tia public api
+    # extract latest posts from tia public api
     extract_latest_posts = PythonOperator(
         task_id="extract_latest_posts",
         python_callable=save_latest_posts_to_json
     )
 
+    # transform posts data
+    transform_posts_data = PythonOperator(
+        task_id="transform_posts_data",
+        python_callable=transform_posts_json
+    )
+
+    # load posts json to postgres
+    load_posts_data = PythonOperator(
+        task_id="load_posts_data",
+        python_callable=load_posts_data_to_postgres
+    )
+
     # streams
-    is_tia_public_api_accessible >> extract_latest_posts
+    is_tia_public_api_accessible >> extract_latest_posts >> transform_posts_data
---
+++ docker/postgres/init_db_tables.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE TABLE IF NOT EXISTS public.posts (
+        id INT NOT NULL,
+        date_gmt VARCHAR NOT NULL,
+        modified_gmt VARCHAR NOT NULL,
+        title VARCHAR NOT NULL,
+        slug VARCHAR NOT NULL,
+        status VARCHAR NOT NULL,
+        type VARCHAR NOT NULL,
+        link VARCHAR NOT NULL,
+        content TEXT NOT NULL,
+        vsitems JSON NULL,
+        live_items JSON NULL,
+        excerpt TEXT NOT NULL,
+        author JSON NULL,
+        editor VARCHAR NOT NULL,
+        comment_status VARCHAR NOT NULL,
+        comments_count INT NOT NULL,
+        comments JSON NULL,
+        featured_image JSON NULL,
+        post_images JSON NULL,
+        seo JSON NULL,
+        categories JSON NULL,
+        tags JSON NULL,
+        companies JSON NULL,
+        is_sponsored BOOLEAN NOT NULL,
+        sponsor JSON NULL,
+        is_partnership BOOLEAN NOT NULL,
+        external_scripts JSON NULL,
+        show_ads BOOLEAN NOT NULL,
+        is_subscriber_exclusive BOOLEAN NOT NULL,
+        is_paywalled BOOLEAN NOT NULL,
+        is_inappbrowser BOOLEAN NOT NULL,
+        read_time INT NOT NULL,
+        word_count INT NULL
+  );
+EOSQL
