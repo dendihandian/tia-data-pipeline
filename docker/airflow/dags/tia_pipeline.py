b2a1421f6bacc39a3c3cb9a946413293aa6dec6c
Dendi Handian <contact.dendi@yahoo.com>
2022-06-08 16:09:11 +0700

    improve efficiency

--- docker/airflow/dags/tia_pipeline.py
+++ docker/airflow/dags/tia_pipeline.py
@@ -1,17 +1,14 @@
-import airflow
-import requests
 import json
-import pandas as pd
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.sensors.http_sensor import HttpSensor
-from airflow.models.connection import Connection
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
-from sqlalchemy import create_engine
+from airflow.utils.dates import days_ago
 
 default_args = {
     "owner": "Airflow",
-    "start_date": airflow.utils.dates.days_ago(1),
+    "start_date": days_ago(1),
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
@@ -21,9 +18,10 @@ default_args = {
 }
 
 def save_latest_posts_to_json():
+    from requests import get
     json_fname = datetime.now().strftime("%Y-%m-%d-%H") + ".json"
     with open(f'/opt/airflow/json/posts/{json_fname}', 'w') as outfile:
-        r = requests.get('https://www.techinasia.com/wp-json/techinasia/2.0/posts?page=1&per_page=30', headers={'Content-Type': 'application/json', 'User-Agent': 'Airflow'})
+        r = get('https://www.techinasia.com/wp-json/techinasia/2.0/posts?page=1&per_page=30', headers={'Content-Type': 'application/json', 'User-Agent': 'Airflow'})
         outfile.write(json.dumps(r.json()['posts']))
 
 def transform_posts_json():
@@ -54,6 +52,9 @@ def transform_posts_json():
         outfile.write(json.dumps(data))
 
 def load_posts_data_to_postgres():
+    import pandas as pd
+    from sqlalchemy import create_engine
+
     json_fname = datetime.now().strftime("%Y-%m-%d-%H") + ".json"
     posts = pd.read_json(f"/opt/airflow/json/posts/{json_fname}")
 
@@ -61,7 +62,7 @@ def load_posts_data_to_postgres():
     posts.to_sql('posts', con=engine, if_exists='append', index=False)
 
 
-with DAG(dag_id="tia_pipeline", schedule_interval="*/5 * * * *", default_args=default_args, catchup=False) as dag:
+with DAG(dag_id="tia_pipeline", schedule_interval="*/2 * * * *", default_args=default_args, catchup=False) as dag:
 
     # check if the tia public api is accessible
     is_tia_public_api_accessible = HttpSensor(
@@ -91,8 +92,24 @@ with DAG(dag_id="tia_pipeline", schedule_interval="*/5 * * * *", default_args=de
         python_callable=load_posts_data_to_postgres
     )
 
-    # remove json file
-    # remove duplicates using postgresql query
+    # remove duplication using postgres
+    # remove_posts_duplication = PostgresOperator(
+    #     task_id="remove_posts_duplication",
+    #     sql="""
+    #         WITH cte AS (
+    #             SELECT 
+    #                 *, 
+    #                 ROW_NUMBER() OVER (
+    #                     PARTITION BY id
+    #                 ) row_num
+    #             FROM 
+    #                 public.posts
+    #         )
+    #         DELETE FROM cte
+    #         WHERE row_num > 1;
+    #     """
+    # )
 
     # streams
     is_tia_public_api_accessible >> extract_latest_posts >> transform_posts_data >> load_posts_data
+    # load_posts_data >> remove_posts_duplication
