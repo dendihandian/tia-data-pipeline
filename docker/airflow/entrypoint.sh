a21147678e050c70b12140bb48b7e8bb54420aeb
Dendi Handian <contact.dendi@yahoo.com>
2022-06-09 01:12:26 +0700

    add tia_postgres connection

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -9,8 +9,9 @@ airflow scheduler &> /dev/null &
 # Create user
 airflow users create -u admin -p admin -r Admin -e admin@admin.com -f admin -l admin
 
-# Create TIA API connection
+# Create custom connection for dags and tasks
 airflow connections add tia_api --conn-type http --conn-host https://www.techinasia.com/wp-json/techinasia/2.0/
+airflow connections add tia_postgres --conn-type postgres --conn-host postgres --conn-port 5432 --conn-schema airflow_db --conn-login airflow --conn-password airflow
 
 # Run the web server in foreground (for docker logs)
 exec airflow webserver
